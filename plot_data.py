2729fde9e1e76b50ec5a3f49c45a8f0fd8d9b77f
Edison Neto <ednetoali@gmail.com>
2018-04-22 20:39:01 -0300

    Changed the plot of coins, and updated data

--- plot_data.py
+++ plot_data.py
@@ -2,12 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 
-data = pd.read_csv('data/ethereum_data.csv')
+ethereum_data = pd.read_csv('data/ethereum_data.csv')
 
 fig, axis = plt.subplots()
 
-axis.plot(data['date'], data['price'], 'r')
-axis.title("Ethereum")
+axis.plot(ethereum_data['date'], ethereum_data['price'], 'r')
 axis.set_ylabel('Price (Dollars)')
 axis.set_xlabel('Date')
+
+doge_data = pd.read_csv('data/dogecoin_data.csv')
+
+axis.plot(doge_data['date'], doge_data['price'], 'r')
 plt.show()
