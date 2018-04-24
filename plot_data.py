e3d249ef9c9f95e58b4a3523d7445d5daa69a45e
Edison Neto <ednetoali@gmail.com>
2018-04-24 18:58:19 -0300

    Fixed plot problems, and updated data

--- plot_data.py
+++ plot_data.py
@@ -9,8 +9,5 @@ fig, axis = plt.subplots()
 axis.plot(ethereum_data['date'], ethereum_data['price'], 'r')
 axis.set_ylabel('Price (Dollars)')
 axis.set_xlabel('Date')
-
-doge_data = pd.read_csv('data/dogecoin_data.csv')
-
-axis.plot(doge_data['date'], doge_data['price'], 'r')
+plt.grid()
 plt.show()
