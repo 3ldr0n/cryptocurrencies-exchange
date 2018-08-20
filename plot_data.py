b72dc5b1ce9a1eb080ace258daeda1d7654f8a69
Edison Neto <ednetoali@gmail.com>
2018-08-20 01:33:33 -0300

    Update plot, fixed some issues with date

--- plot_data.py
+++ plot_data.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from matplotlib.dates import DateFormatter
+btc_data = pd.read_csv("data/bitcoin_data.csv")
+eth_data = pd.read_csv("data/ethereum_data.csv")
+pd.to_numeric(btc_data["price"])
+pd.to_numeric(eth_data["price"])
 
-data = pd.read_csv("data/bitcoin_data.csv")
-pd.to_numeric(data["price"])
+btc_data["date"] = pd.to_datetime(
+    btc_data["date"], format="%m/%d/%y-%H:%M:%S")
+eth_data["date"] = pd.to_datetime(
+    eth_data["date"], format="%m/%d/%y-%H:%M:%S")
 
 axis = plt.axes()
 
-axis.plot(data["date"], data["price"])
-axis.set_xticks([
-    data["date"][0],
-    data["date"][int(len(data["date"])/2)],
-    data["date"][len(data["date"])-1]])
+axis.plot(btc_data["date"], btc_data["price"])
+axis.plot(eth_data["date"], eth_data["price"])
+axis.set_title("Bitcoin and Ethereum price since " + str(btc_data["date"][0]))
+axis.set_ylabel("Price (USD)")
+axis.set_xlabel("Date")
 
-axis.grid()
 plt.show()
