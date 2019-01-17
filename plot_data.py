60bdb2aaf0ee20eded250ddc9e523370969a2bcf
Edison Neto <ednetoali@gmail.com>
2019-01-17 14:34:02 -0200

    Update plot script

--- plot_data.py
+++ plot_data.py
@@ -1,21 +1,20 @@
+import sys
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-btc_data = pd.read_csv("data/bitcoin_data.csv")
-eth_data = pd.read_csv("data/ethereum_data.csv")
-pd.to_numeric(btc_data["price"])
-pd.to_numeric(eth_data["price"])
+crypto = sys.argv[1]
 
-btc_data["date"] = pd.to_datetime(
-    btc_data["date"], format="%m/%d/%y-%H:%M:%S")
-eth_data["date"] = pd.to_datetime(
-    eth_data["date"], format="%m/%d/%y-%H:%M:%S")
+data = pd.read_csv("data/{}_data.csv".format(crypto))
+pd.to_numeric(data["price"])
+
+data["date"] = pd.to_datetime(
+    data["date"], format="%m/%d/%y-%H:%M:%S")
 
 axis = plt.axes()
 
-axis.plot(btc_data["date"], btc_data["price"])
-axis.plot(eth_data["date"], eth_data["price"])
-axis.set_title("Bitcoin and Ethereum price since " + str(btc_data["date"][0]))
+axis.plot(data["date"], data["price"])
+axis.set_title("{} price since {}".format(crypto, data["date"][0]))
 axis.set_ylabel("Price (USD)")
 axis.set_xlabel("Date")
 
